ca407c184257b77c59e2461cdffdf29ea1abf3fd
seansql <shawnq2116@gmal.com>
2020-09-21 13:47:33 -0400

    Add project files.

---
+++ RestaurantRaterRB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRaterRB.Startup))]
+namespace RestaurantRaterRB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
