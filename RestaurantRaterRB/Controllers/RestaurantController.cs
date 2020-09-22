8c8bb78614241bb2b1f8a19982ecf072febf7a07
seansql <shawnq2116@gmal.com>
2020-09-22 16:44:13 -0400

    Added details view

--- RestaurantRaterRB/Controllers/RestaurantController.cs
+++ RestaurantRaterRB/Controllers/RestaurantController.cs
@@ -102,5 +102,20 @@ namespace RestaurantRaterRB.Controllers
             return View(restaurant);
         }
 
+        // Get: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
     }
 }
